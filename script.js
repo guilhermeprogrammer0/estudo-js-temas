a77be6e82a27238cc7ec97f06637c278965bcf72
Guilherme Souza <guilherme003souza@gmail.com>
2024-04-09 10:46:44 -0300

    Enviando Projeto ao github

---
+++ script.js
@@ -0,0 +1,52 @@
+let temaCard = localStorage.getItem("tema");
+let botoes = document.querySelectorAll(".btnCores");
+let cardBody = document.querySelector(".card-body");
+botoes.forEach((botao)=>{
+    botao.addEventListener('click', mudaCor);
+})
+function mudaCor(event){
+    cardBody.className = 'card-body'
+    switch(event.target.className.substring(9,30)){
+        case "preto":
+            cardBody.classList.add("preto");
+            localStorage.setItem("tema","preto");
+            break;
+            case "azul":
+                cardBody.classList.add("azul");
+                localStorage.setItem("tema","azul");
+                break;
+                case "vermelho":
+                    cardBody.classList.add("vermelho");
+                    localStorage.setItem("tema","vermelho");
+                    break;
+                    case "verde":
+                        cardBody.classList.add("verde");
+                        localStorage.setItem("tema","verde");
+                        break;
+                        case "branco":
+                            cardBody.classList.add("branco");
+                            localStorage.setItem("tema","branco");
+                            break;
+                            default:
+                                break;
+    }
+}
+switch(temaCard){
+    case "preto":
+            cardBody.classList.add("preto");
+            break;
+            case "azul":
+                cardBody.classList.add("azul");
+                break;
+                case "vermelho":
+                    cardBody.classList.add("vermelho");
+                    break;
+                    case "verde":
+                        cardBody.classList.add("verde");
+                        break;
+                        case "branco":
+                            cardBody.classList.add("branco");
+                            break;
+                            default:
+                                break;
+    }
